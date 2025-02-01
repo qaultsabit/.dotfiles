0743c2dd5f782e48b9d7eb320d91adbbebfe7f43
tibast <qaultsabitm@gmail.com>
2025-02-02 01:27:29 +0700

    remove todo highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -86,14 +86,6 @@ require('lazy').setup({
     end,
   },
 
-  -- Highlight todo, notes, etc in comments
-  {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-  },
-
   { import = 'plugins' },
 }, {
   ui = {
