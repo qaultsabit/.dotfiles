ddb505be961d254fe7d3a4e681d89356b31ebee3
tibast <qaultsabitm@gmail.com>
2025-02-15 17:36:30 +0700

    shorten

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,53 +1,36 @@
--- nvim-tree
 return {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        local screen_w, screen_h = vim.o.columns, vim.o.lines
+        local win_w, win_h = math.floor(screen_w * 0.5), math.floor(screen_h * 0.8)
+        local row, col = ((screen_h - win_h) / 2) - 1, (screen_w - win_w) / 2
+
         require("nvim-tree").setup({
-            filters = {
-                dotfiles = false,
-                custom = { "^.git$" },
-            },
+            filters = { dotfiles = false, custom = { "^.git$" } },
             disable_netrw = true,
             view = {
                 float = {
                     enable = true,
-                    open_win_config = function()
-                        local screen_w = vim.o.columns
-                        local screen_h = vim.o.lines
-                        local win_w = math.floor(screen_w * 0.5)
-                        local win_h = math.floor(screen_h * 0.8)
-                        local row = ((screen_h - win_h) / 2) - 1
-                        local col = (screen_w - win_w) / 2
-                        return {
-                            relative = "editor",
-                            border = "solid",
-                            width = win_w,
-                            height = win_h,
-                            row = row,
-                            col = col,
-                        }
-                    end,
+                    open_win_config = {
+                        relative = "editor",
+                        border = "solid",
+                        width = win_w,
+                        height = win_h,
+                        row = row,
+                        col = col }
                 },
-                width = function()
-                    return math.floor(vim.o.columns * 0.5)
-                end,
+                width = function() return win_w end,
             },
             renderer = {
                 root_folder_label = false,
                 highlight_git = true,
                 group_empty = true,
-                indent_markers = { enable = true },
+                indent_markers = { enable = true }
             },
-            actions = {
-                open_file = {
-                    window_picker = { enable = false },
-                },
-            },
+            actions = { open_file = { window_picker = { enable = false } } },
         })
     end,
 }
