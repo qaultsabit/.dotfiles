b77fdff7a420644a84c91446b9efe6d892d320d5
tibast <qaultsabitm@gmail.com>
2025-03-09 02:52:03 +0700

    fterm

--- .config/nvim/lua/plugins/fterm.lua
+++ .config/nvim/lua/plugins/fterm.lua
@@ -2,7 +2,7 @@ return {
   "numToStr/FTerm.nvim",
   config = function()
     require("FTerm").setup({
-      border = "none",
+      border = "solid",
       hl = "NormalFloat",
       dimensions = {
         height = 0.85,
