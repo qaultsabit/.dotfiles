e8cdbab4bb10801add5bb7445e056f56a93d8f9b
tibast <qaultsabitm@gmail.com>
2025-03-09 21:20:10 +0700

    edit visual highlight

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -6,7 +6,12 @@ return {
     require("tokyonight").setup({
       style = "storm",
       terminal_colors = true,
+      on_highlights = function(highlights, colors)
+        highlights.Visual = { bg = colors.blue, fg = colors.black }
+        highlights.TelescopeSelection = { bg = colors.bg_highlight, fg = colors.fg, bold = true }
+        highlights.TelescopeMatching = { fg = colors.orange }
+      end,
     })
-    vim.cmd([[colorscheme tokyonight]])
+    vim.cmd("colorscheme tokyonight")
   end,
 }
