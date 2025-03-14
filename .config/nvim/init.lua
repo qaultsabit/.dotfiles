308ddea21d073d22b8795bbbdabee5916651ed22
tibast <qaultsabitm@gmail.com>
2025-03-15 00:43:47 +0700

    wtf is 256 colors?

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.g.have_nerd_font = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
